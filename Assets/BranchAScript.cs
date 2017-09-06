4a3ff8c0046540809f063d129419523127282ce2
seungchan.jeong <seungchan.jeong@binaree.net>
2017-09-06 09:52:50 +0900

    Commit

--- Assets/BranchAScript.cs
+++ Assets/BranchAScript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class BranchAScript : MonoBehaviour {
 
+	public float startedTime;
+
 	// Use this for initialization
 	void Start () {
+		startedTime = Time.time;
 		Debug.Log("Hi I just started");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.Log("Elapsed Time : " + (Time.time-startedTime));
 	}
 }
