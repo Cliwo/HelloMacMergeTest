506762d20edc8fd76c50c69c572fc67ccb3dc8a0
seungchan.jeong <seungchan.jeong@binaree.net>
2017-09-06 09:47:23 +0900

    first commit

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizon = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		transform.Translate (new Vector3 (horizon, vertical, 0));
+	}
+}
